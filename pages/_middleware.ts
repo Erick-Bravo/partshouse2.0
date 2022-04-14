b1974c9fed98601adee65caba05f97988c1e2d19
Erick Bravo <erickbravo@MBP.local>
2022-04-13 17:28:47 -0700

    _middleware

---
+++ pages/_middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+//if not verified, and user tries to access these pages, it will re-route to signin
+const signInPages = ["/", "/playlist", "/library"];
+
+export default function middleware(req) {
+  if (signInPages.find((p) => p === req.nextUrl.pathname)) {
+    const token = req.cookies.TRAX_ACCESS_TOKEN;
+
+    if (!token) {
+      return NextResponse.redirect("/signin");
+    }
+  }
+}
