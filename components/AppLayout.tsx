2f45fe118d5369e40de2798d0ba7bb1fe2ef7138
Erick Bravo <erickbravo@MBP.lan>
2022-05-15 17:49:54 -0700

    navbar layout starter

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,12 +1,12 @@
 import { Box } from "@chakra-ui/layout";
 import NextImage from "next/image";
-import SideBar from "./SideBar";
+import NavBar from "./NavBar";
 
 const AppLayout = ({ children }) => {
   return (
-    <Box width="100vw" height="100%">
-      <Box position="absolute" top="0" width="250px" left="0">
-        <SideBar />
+    <Box width="100vw" height="100%" >
+      <Box position="absolute" top="0" width="100vw" left="0">
+        <NavBar />
       </Box>
       <Box>
         {children}
