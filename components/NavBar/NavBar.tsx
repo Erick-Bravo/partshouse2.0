1354c1808bc7445769672399d3b5ad570ec20f92
Erick Bravo <erickbravo@MBP.lan>
2022-05-16 22:40:29 -0700

    NavBar responsive with working open/close hamburger menu

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -24,20 +24,21 @@ import NextImage from "next/image";
 import NextLink from "next/link";
 import { MdSupervisorAccount } from "react-icons/md";
 import { useState } from "react";
+import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 
 const NavBar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
 
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
+  const [display, changeDisplay] = useState("none");
+
 
   const edgeSquareWidth = "300px";
 
   return (
     <Flex>
       <Flex pos="fixed" top="1rem" right="1rem" align="center">
-        <Flex>
+        <Flex display={["none", "none", "flex", "flex"]}>
           <NextLink href="/" passHref>
             <Button as="a" variant="ghost" aria-label="Home" my="5" w="100%">
               Home
@@ -56,7 +57,65 @@ const NavBar = () => {
             </Button>
           </NextLink>
         </Flex>
-        <Switch color="green" isChecked={isDark} onChange={toggleColorMode} paddingLeft="10px" />
+
+        <IconButton
+          aria-label="Open Menu"
+          size="lg"
+          mr={2}
+          icon={<HamburgerIcon />}
+          display={["flex", "flex", "none", "none"]}
+          onClick={() => changeDisplay("flex")}
+        />
+
+        <Switch
+          color="green"
+          isChecked={isDark}
+          onChange={toggleColorMode}
+          paddingLeft="10px"
+        />
+      </Flex>
+
+      <Flex
+        w="100vw"
+        bgColor="gray.50"
+        zIndex={20}
+        h="100vh"
+        pos="fixed"
+        top="0"
+        left="0"
+        overflow="auto"
+        flexDir="column"
+        display={display}
+      >
+        <Flex justify="flex-end" >
+          <IconButton
+            mt={2}
+            mr={2}
+            aria-label="Close Menu"
+            size="lg"
+            icon={<CloseIcon />}
+            onClick={() => changeDisplay("none")}
+          />
+        </Flex>
+        <Flex flexDir="column" align="center">
+          <NextLink href="/" passHref>
+            <Button as="a" variant="ghost" aria-label="Home" my="5" w="100%">
+              Home
+            </Button>
+          </NextLink>
+
+          <NextLink href="/about" passHref>
+            <Button as="a" variant="ghost" aria-label="Home" my="5" w="100%">
+              About
+            </Button>
+          </NextLink>
+
+          <NextLink href="/" passHref>
+            <Button as="a" variant="ghost" aria-label="Home" my="5" w="100%">
+              Logout
+            </Button>
+          </NextLink>
+        </Flex>
       </Flex>
     </Flex>
   );
